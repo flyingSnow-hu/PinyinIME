ed67901fc7004ec4da0aff52ba0d7eb8a7574e5d
flyingsnow <flyingsnow.hu@gmail.com>
2019-03-21 04:23:24 +0800

    基本可以运行

---
+++ app/src/main/java/com/flyingsnow/inputmethod/pinyin/KeyCodeEx.java
@@ -0,0 +1,8 @@
+package com.flyingsnow.inputmethod.pinyin;
+
+public final class KeyCodeEx {
+    public static final int KEYCODE_EX               = 300;
+    public static final int KEYCODE_ZH               = 301;
+    public static final int KEYCODE_CH               = 302;
+    public static final int KEYCODE_SH               = 303;
+}
